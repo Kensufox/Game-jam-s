a2ab38d0f7758bbc99693f03d6d1448f7ea60e20
William <williapro0@gmail.com>
2025-04-14 14:26:46 +0200

    level 1 gameplay finit

--- game_jam/Perso/scripts/PlayerOne.gd
+++ game_jam/Perso/scripts/PlayerOne.gd
@@ -26,3 +26,7 @@ func set_animation(direction):
 #		animated_sprite_2d.play("walk")
 #	else:
 #		animated_sprite_2d.play("idle")
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
