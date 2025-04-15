cae145452ffb0b1bf182199360aab376cdcc7059
Kensufox <pokenfox@gmail.com>
2025-04-15 14:03:55 +0200

    correct pressure plate collision

--- game_jam/level/scripts/PressurePlate.gd
+++ game_jam/level/scripts/PressurePlate.gd
@@ -1,16 +1,13 @@
 extends Node
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "LeftPlayer":
-		$"../Water/Bridge/Bridge".set_deferred("disabled", true)
-		print($"../Water/Bridge/Bridge".disabled)
-	#else:
-		
+	#var path = "../"+name.replace("PressurePlate", "")+"/Bridge/Bridge"
 	
-
+	$"../Bridge/Bridge".set_deferred("disabled", not $"../Bridge/Bridge".disabled)
+	print($"../Bridge/Bridge".disabled)
+	
 func _on_body_exited(body: Node2D) -> void:
-	$"../Water/Bridge/Bridge".set_deferred("disabled", false)
-	print($"../Water/Bridge/Bridge".disabled)
-
+	#var path = "../"+name.replace("PressurePlate", "")+"/Bridge/Bridge"
 	
-#$"../Water/Bridge/Bridge"$ "../../../../Right/RightArea/Tiles/Lava/Bridge/Bridge"
+	$"../Bridge/Bridge".set_deferred("disabled", not $"../Bridge/Bridge".disabled)
+	print($"../Bridge/Bridge".disabled)
