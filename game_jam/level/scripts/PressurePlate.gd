7d7065aabbd0a316e1e0b82d12f431f1b884e56c
Kensufox <pokenfox@gmail.com>
2025-04-16 22:36:08 +0200

    apply bridge animation

--- game_jam/level/scripts/PressurePlate.gd
+++ game_jam/level/scripts/PressurePlate.gd
@@ -1,13 +1,21 @@
 extends Node
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var leftbridge: CollisionShape2D  = $"../../../../../Left/LeftArea/Tiles/Interactif/Bridge/Bridge"
+@onready var rightbridge: CollisionShape2D = $"../../../../../Right/RightArea/Tiles/Interactif/Bridge/Bridge"
+@onready var leftsprite: Sprite2D  = $"../../../../../Left/LeftArea/Tiles/Interactif/Bridge/Sprite2D"
+@onready var rightsprite: Sprite2D = $"../../../../../Right/RightArea/Tiles/Interactif/Bridge/Sprite2D"
 
 func _on_body_entered(body: Node2D) -> void:
-	$"../../../../../../Left/LeftArea/Tiles/Interactif/Link1/Bridge/Bridge".set_deferred("disabled", true)
-	$"../../../../../../Right/RightArea/Tiles/Interactif/Link1/Bridge/Bridge".set_deferred("disabled", false)
+	leftbridge.set_deferred("disabled", true)
+	rightbridge.set_deferred("disabled", false)
+	leftsprite.texture = load("res://asset/element/wood_bridge_32x32.png")
+	rightsprite.texture = load("res://asset/element/wood_bridge_ghost_32x32.png")
 	sprite.texture = load("res://asset/element/blank_button_pressed_32x32.png")
 	
 
 func _on_body_exited(body: Node2D) -> void:
-	$"../../../../../../Left/LeftArea/Tiles/Interactif/Link1/Bridge/Bridge".set_deferred("disabled", false)
-	$"../../../../../../Right/RightArea/Tiles/Interactif/Link1/Bridge/Bridge".set_deferred("disabled", true)
+	leftbridge.set_deferred("disabled", false)
+	rightbridge.set_deferred("disabled", true)
+	leftsprite.texture = load("res://asset/element/wood_bridge_ghost_32x32.png")
+	rightsprite.texture = load("res://asset/element/wood_bridge_32x32.png")
 	sprite.texture = load("res://asset/element/blank_button_32x32.png")
