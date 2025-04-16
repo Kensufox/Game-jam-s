7e6cc6b19497e07b36bb9197497c56bc9b8b0a13
Kensufox <pokenfox@gmail.com>
2025-04-16 22:16:48 +0200

    add animation to the button in lvl2

--- game_jam/level/scripts/PressurePlate.gd
+++ game_jam/level/scripts/PressurePlate.gd
@@ -1,9 +1,13 @@
 extends Node
+@onready var sprite: Sprite2D = $Sprite2D
 
 func _on_body_entered(body: Node2D) -> void:
 	$"../../../../../../Left/LeftArea/Tiles/Interactif/Link1/Bridge/Bridge".set_deferred("disabled", true)
 	$"../../../../../../Right/RightArea/Tiles/Interactif/Link1/Bridge/Bridge".set_deferred("disabled", false)
+	sprite.texture = load("res://asset/element/blank_button_pressed_32x32.png")
+	
 
 func _on_body_exited(body: Node2D) -> void:
 	$"../../../../../../Left/LeftArea/Tiles/Interactif/Link1/Bridge/Bridge".set_deferred("disabled", false)
 	$"../../../../../../Right/RightArea/Tiles/Interactif/Link1/Bridge/Bridge".set_deferred("disabled", true)
+	sprite.texture = load("res://asset/element/blank_button_32x32.png")
