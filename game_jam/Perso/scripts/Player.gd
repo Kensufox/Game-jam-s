ce5674501e2098d35a736c377be7018b1448ef02
Kensufox <pokenfox@gmail.com>
2025-04-17 11:33:53 +0200

    :) bug fixed

--- game_jam/Perso/scripts/Player.gd
+++ game_jam/Perso/scripts/Player.gd
@@ -2,32 +2,29 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-var direction := Vector2.ZERO
-	
-func _ready():
-	TickManager.tick.connect(_on_tick)
+var time_since_move := 0.0
+var move_cooldown := 0.1
 
-func _physics_process(delta):
-	direction.x = Input.get_axis("ui_left", "ui_right")
-	direction.y = Input.get_axis("ui_up", "ui_down")
+func _physics_process(delta: float) -> void:
+	time_since_move += delta
+	if time_since_move >= move_cooldown:
+		var direction := Vector2.ZERO
+		direction.x = Input.get_axis("ui_left", "ui_right")
+		direction.y = Input.get_axis("ui_up", "ui_down")
 
-	if direction.x != 0:
-		direction.y = 0
-	elif direction.y != 0:
-		direction.x = 0
+		if direction.x != 0:
+			direction.y = 0
+		elif direction.y != 0:
+			direction.x = 0
 			
-	if VariableGlobale.invert_controls_left:
-		direction *= -1
-		
-func _on_tick():
-	if direction != Vector2.ZERO:
-		move_and_collide(direction.normalized() * 32)
-		position = Vector2(round(position.x/32)*32, round(position.y/32)*32)
-		set_animation(direction)
-		print("player : ", name, " moved on tick")
-	
+		if VariableGlobale.invert_controls_left:
+			direction *= -1
 
-
+		if direction != Vector2.ZERO:
+			move_and_collide(direction.normalized() * 32)
+			position = Vector2(round(position.x/32)*32, round(position.y/32)*32)
+			time_since_move = 0.0
+			set_animation(direction)
 
 func set_animation(direction):
 	if direction.x > 0:
