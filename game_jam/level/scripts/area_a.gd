3415f2ed7a7464999f24d44f1f7b7d2fd0953103
Kensufox <pokenfox@gmail.com>
2025-04-18 10:46:52 +0200

    change the changing level method

--- game_jam/level/scripts/area_a.gd
+++ game_jam/level/scripts/area_a.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	VariableGlobale.zone_gauche = true
+	VariableGlobale.left_area = true
 	await get_tree().create_timer(0.1).timeout
-	VariableGlobale.redirection()
+	VariableGlobale.redirection($"../../../..".name)
 
 func _on_body_exited(body: Node2D) -> void:
-	VariableGlobale.zone_gauche = false
+	VariableGlobale.left_area = false
--- game_jam/level/scripts/area_b.gd
+++ game_jam/level/scripts/area_b.gd
@@ -1,10 +1,9 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	VariableGlobale.zone_droite = true
+	VariableGlobale.right_area = true
 	await get_tree().create_timer(0.1).timeout
-	VariableGlobale.redirection()
-
+	VariableGlobale.redirection($"../../../..".name)
 
 func _on_body_exited(body: Node2D) -> void:
-	VariableGlobale.zone_droite = false
+	VariableGlobale.right_area = false
