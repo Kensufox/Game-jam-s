adcb3ab0f7c2171b2b536abca001bebd8d61b9d9
Kensufox <pokenfox@gmail.com>
2025-04-18 10:55:05 +0200

    del areab.gd and make areaa.gd into end_level.gd

--- game_jam/level/scripts/area_a.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func _on_body_entered(body: Node2D) -> void:
-	VariableGlobale.left_area = true
-	await get_tree().create_timer(0.1).timeout
-	VariableGlobale.redirection($"../../../..".name)
-
-func _on_body_exited(body: Node2D) -> void:
-	VariableGlobale.left_area = false
